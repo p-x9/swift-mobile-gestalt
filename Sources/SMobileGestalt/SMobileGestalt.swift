eb81c78b9d746cf5cbe263fb2ed3856062689ae0
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-08-27 22:16:34 +0900

    add `SMGSetAnswer`

--- Sources/SMobileGestalt/SMobileGestalt.swift
+++ Sources/SMobileGestalt/SMobileGestalt.swift
@@ -8,6 +8,15 @@ public func SMGCopyAnswer(_ property: MobileGestaltKeyProtocol) -> CFTypeRef? {
     return MGCopyAnswer(property.rawValue as CFString)?.takeRetainedValue()
 }
 
+@discardableResult
+public func SMGSetAnswer(question: MobileGestaltKeyProtocol, answer: CFTypeRef) -> Int {
+    let gestalt = dlopen("/usr/lib/libMobileGestalt.dylib", RTLD_GLOBAL | RTLD_LAZY)
+    typealias MGSetAnswerFunc = @convention(c) (CFString, CFTypeRef) -> Int
+    let MGSetAnswer = unsafeBitCast(dlsym(gestalt, "MGSetAnswer"), to: MGSetAnswerFunc.self)
+
+    return MGSetAnswer(question.rawValue as CFString, answer)
+}
+
 public func SMGCopyAnswerAsString(_ property: MobileGestaltKeyProtocol) -> String? {
     if let propertyList: CFTypeRef = SMGCopyAnswer(property) {
         switch propertyList {
