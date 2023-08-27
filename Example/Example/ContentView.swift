9f203ec5e70062a65c395c0c6f847b22b2a19a8b
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-08-27 22:07:10 +0900

    add Example App

---
+++ Example/Example/ContentView.swift
@@ -0,0 +1,192 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by p-x9 on 2023/08/27.
+//  
+//
+
+import SwiftUI
+import SMobileGestalt
+
+struct ContentView: View {
+    var body: some View {
+        List {
+            Group {
+                identifyingSection
+                bluetoothSection
+                batterySection
+                basebandSection
+                telephonySection
+            }
+            Group {
+                deviceSection
+                capabilitySection
+                regionalSection
+                wirelessSection
+                faceTimeSection
+            }
+            moreDeviceSection
+        }
+    }
+
+    var identifyingSection: some View {
+        Section {
+            ForEach(MobileGestaltKey.Identifying.allCases, id: \.self) { key in
+                HStack {
+                    Text(key.rawValue)
+                    Spacer()
+                    Text(SMGCopyAnswerString(key) ?? "nil")
+                }
+            }
+        } header: {
+            Text("Identifying")
+        }
+    }
+
+    var bluetoothSection: some View {
+        Section {
+            ForEach(MobileGestaltKey.Bluetooth.allCases, id: \.self) { key in
+                HStack {
+                    Text(key.rawValue)
+                    Spacer()
+                    Text(SMGCopyAnswerString(key) ?? "nil")
+                }
+            }
+        } header: {
+            Text("Bluetooth")
+        }
+    }
+
+    var batterySection: some View {
+        Section {
+            ForEach(MobileGestaltKey.Battery.allCases, id: \.self) { key in
+                HStack {
+                    Text(key.rawValue)
+                    Spacer()
+                    Text(SMGCopyAnswerString(key) ?? "nil")
+                }
+            }
+        } header: {
+            Text("Battery")
+        }
+    }
+
+    var basebandSection: some View {
+        Section {
+            ForEach(MobileGestaltKey.Baseband.allCases, id: \.self) { key in
+                HStack {
+                    Text(key.rawValue)
+                    Spacer()
+                    Text(SMGCopyAnswerString(key) ?? "nil")
+                }
+            }
+        } header: {
+            Text("Baseband")
+        }
+    }
+
+    var telephonySection: some View {
+        Section {
+            ForEach(MobileGestaltKey.Telephony.allCases, id: \.self) { key in
+                HStack {
+                    Text(key.rawValue)
+                    Spacer()
+                    Text(SMGCopyAnswerString(key) ?? "nil")
+                }
+            }
+        } header: {
+            Text("Telephony")
+        }
+    }
+
+    var deviceSection: some View {
+        Section {
+            ForEach(MobileGestaltKey.Device.allCases, id: \.self) { key in
+                HStack {
+                    Text(key.rawValue)
+                    Spacer()
+                    Text(SMGCopyAnswerString(key) ?? "nil")
+                }
+            }
+        } header: {
+            Text("Device")
+        }
+    }
+
+    var capabilitySection: some View {
+        Section {
+            ForEach(MobileGestaltKey.Capability.allCases, id: \.self) { key in
+                HStack {
+                    Text(key.rawValue)
+                    Spacer()
+                    Text(SMGCopyAnswerString(key) ?? "nil")
+                }
+            }
+        } header: {
+            Text("Capability")
+        }
+    }
+
+    var regionalSection: some View {
+        Section {
+            ForEach(MobileGestaltKey.RegionalBehaviour.allCases, id: \.self) { key in
+                HStack {
+                    Text(key.rawValue)
+                    Spacer()
+                    Text(SMGCopyAnswerString(key) ?? "nil")
+                }
+            }
+        } header: {
+            Text("Regional")
+        }
+    }
+
+    var wirelessSection: some View {
+        Section {
+            ForEach(MobileGestaltKey.Wireless.allCases, id: \.self) { key in
+                HStack {
+                    Text(key.rawValue)
+                    Spacer()
+                    Text(SMGCopyAnswerString(key) ?? "nil")
+                }
+            }
+        } header: {
+            Text("Wireless")
+        }
+    }
+
+    var faceTimeSection: some View {
+        Section {
+            ForEach(MobileGestaltKey.FaceTime.allCases, id: \.self) { key in
+                HStack {
+                    Text(key.rawValue)
+                    Spacer()
+                    Text(SMGCopyAnswerString(key) ?? "nil")
+                }
+            }
+        } header: {
+            Text("FaceTime")
+        }
+    }
+
+    var moreDeviceSection: some View {
+        Section {
+            ForEach(MobileGestaltKey.MoreDeviceCapability.allCases, id: \.self) { key in
+                HStack {
+                    Text(key.rawValue)
+                    Spacer()
+                    Text(SMGCopyAnswerString(key) ?? "nil")
+                }
+            }
+        } header: {
+            Text("MoreDeviceCapability")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- Sources/SMobileGestalt/SMobileGestalt.swift
+++ Sources/SMobileGestalt/SMobileGestalt.swift
@@ -1,18 +1,20 @@
 import Foundation
 
-public func SMGCopyAnswer(_ property: MobileGestaltKeyProtocol) -> CFPropertyList? {
+public func SMGCopyAnswer(_ property: MobileGestaltKeyProtocol) -> CFTypeRef? {
     let gestalt = dlopen("/usr/lib/libMobileGestalt.dylib", RTLD_GLOBAL | RTLD_LAZY)
-    typealias MGCopyAnswerFunc = @convention(c) (CFString) -> Optional<Unmanaged<CFPropertyList>>
+    typealias MGCopyAnswerFunc = @convention(c) (CFString) -> Optional<Unmanaged<CFTypeRef>>
     let MGCopyAnswer = unsafeBitCast(dlsym(gestalt, "MGCopyAnswer"), to: MGCopyAnswerFunc.self)
 
     return MGCopyAnswer(property.rawValue as CFString)?.takeRetainedValue()
 }
 
 public func SMGCopyAnswerString(_ property: MobileGestaltKeyProtocol) -> String? {
-    if let propertyList: CFPropertyList = SMGCopyAnswer(property) {
+    if let propertyList: CFTypeRef = SMGCopyAnswer(property) {
         switch propertyList {
         case let v as String:
             return v
+        case let v as Bool:
+            return "\(v)"
         case let v as NSNumber:
             return "\(v)"
         case let v as [Any]:
