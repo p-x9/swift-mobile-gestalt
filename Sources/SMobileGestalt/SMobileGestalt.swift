7a77d10d0f533cb31a238276c46539f22289a75b
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-10-21 01:00:10 +0900

    allow key to be specified as a String

--- Sources/SMobileGestalt/SMobileGestalt.swift
+++ Sources/SMobileGestalt/SMobileGestalt.swift
@@ -1,24 +1,35 @@
 import Foundation
 
-public func SMGCopyAnswer(_ property: MobileGestaltKeyProtocol) -> CFTypeRef? {
+// MARK: - SMGCopyAnswer
+public func SMGCopyAnswer(_ property: String) -> CFTypeRef? {
     let gestalt = dlopen("/usr/lib/libMobileGestalt.dylib", RTLD_GLOBAL | RTLD_LAZY)
     typealias MGCopyAnswerFunc = @convention(c) (CFString) -> Optional<Unmanaged<CFTypeRef>>
     let MGCopyAnswer = unsafeBitCast(dlsym(gestalt, "MGCopyAnswer"), to: MGCopyAnswerFunc.self)
 
-    return MGCopyAnswer(property.rawValue as CFString)?.takeRetainedValue()
+    return MGCopyAnswer(property as CFString)?.takeRetainedValue()
+}
+
+public func SMGCopyAnswer(_ property: MobileGestaltKeyProtocol) -> CFTypeRef? {
+    SMGCopyAnswer(property.rawValue)
 }
 
 public func SMGCopyAnswer(_ property: MobileGestaltKey) -> CFTypeRef? {
-    SMGCopyAnswer(property as any MobileGestaltKeyProtocol)
+    SMGCopyAnswer(property.rawValue)
 }
 
+// MARK: - SMGSetAnswer
 @discardableResult
-public func SMGSetAnswer(question: MobileGestaltKeyProtocol, answer: CFTypeRef) -> Int {
+public func SMGSetAnswer(question: String, answer: CFTypeRef) -> Int {
     let gestalt = dlopen("/usr/lib/libMobileGestalt.dylib", RTLD_GLOBAL | RTLD_LAZY)
     typealias MGSetAnswerFunc = @convention(c) (CFString, CFTypeRef) -> Int
     let MGSetAnswer = unsafeBitCast(dlsym(gestalt, "MGSetAnswer"), to: MGSetAnswerFunc.self)
 
-    return MGSetAnswer(question.rawValue as CFString, answer)
+    return MGSetAnswer(question as CFString, answer)
+}
+
+@discardableResult
+public func SMGSetAnswer(question: MobileGestaltKeyProtocol, answer: CFTypeRef) -> Int {
+    SMGSetAnswer(question: question.rawValue, answer: answer)
 }
 
 @discardableResult
@@ -26,7 +37,8 @@ public func SMGSetAnswer(question: MobileGestaltKey, answer: CFTypeRef) -> Int {
     SMGSetAnswer(question: question as MobileGestaltKeyProtocol, answer: answer)
 }
 
-public func SMGCopyAnswerAsString(_ property: MobileGestaltKeyProtocol) -> String? {
+// MARK: - SMGCopyAnswerAsString
+public func SMGCopyAnswerAsString(_ property: String) -> String? {
     if let answer: CFTypeRef = SMGCopyAnswer(property) {
         switch CFGetTypeID(answer) {
         case CFBooleanGetTypeID():
@@ -49,6 +61,10 @@ public func SMGCopyAnswerAsString(_ property: MobileGestaltKeyProtocol) -> Strin
     return nil
 }
 
+public func SMGCopyAnswerAsString(_ property: MobileGestaltKeyProtocol) -> String? {
+    SMGCopyAnswerAsString(property.rawValue)
+}
+
 public func SMGCopyAnswerAsString(_ property: MobileGestaltKey) -> String? {
-    SMGCopyAnswerAsString(property as MobileGestaltKeyProtocol)
+    SMGCopyAnswerAsString(property.rawValue)
 }
