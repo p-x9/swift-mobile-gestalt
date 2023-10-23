96a6607881b6b5c690a1241f268a08145456a03f
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-10-23 14:13:37 +0900

    refactor example app ui

--- Example/Example/ContentView.swift
+++ Example/Example/ContentView.swift
@@ -10,177 +10,68 @@ import SwiftUI
 import SMobileGestalt
 
 struct ContentView: View {
+    @State var searchText: String = ""
+
     var body: some View {
-        List {
-            Group {
-                identifyingSection
-                bluetoothSection
-                batterySection
-                basebandSection
-                telephonySection
+        NavigationView {
+            List {
+                Group {
+                    section("Device", keys: MobileGestaltKey.Device.allStringKeys)
+                    section("Identifying", keys: MobileGestaltKey.Identifying.allStringKeys)
+
+                    section("Battery", keys: MobileGestaltKey.Battery.allStringKeys)
+                    section("Camera", keys: MobileGestaltKey.Camera.allStringKeys)
+
+                    section("Wireless", keys: MobileGestaltKey.Wireless.allStringKeys)
+                    section("Bluetooth", keys: MobileGestaltKey.Bluetooth.allStringKeys)
+                }
+                Group {
+                    section("Baseband", keys: MobileGestaltKey.Baseband.allStringKeys)
+                    section("Telephony", keys: MobileGestaltKey.Telephony.allStringKeys)
+
+                    section("Software", keys: MobileGestaltKey.Software.allStringKeys)
+
+                    section("Media", keys: MobileGestaltKey.Media.allStringKeys)
+
+                    section("Capability", keys: MobileGestaltKey.Capability.allStringKeys)
+                    section("MoreDeviceCapability", keys: MobileGestaltKey.MoreDeviceCapability.allStringKeys)
+                }
+                Group {
+                    section("RegionalBehavior", keys: MobileGestaltKey.RegionalBehavior.allStringKeys)
+
+                    section("WatchSupport", keys: MobileGestaltKey.WatchSupport.allStringKeys)
+                    section("FaceTime", keys: MobileGestaltKey.FaceTime.allStringKeys)
+                }
+                section("Other", keys: MobileGestaltKey.Other.allStringKeys)
             }
-            Group {
-                deviceSection
-                capabilitySection
-                regionalSection
-                wirelessSection
-                faceTimeSection
-            }
-            moreDeviceSection
         }
+        .searchable(text: $searchText)
     }
 
-    var identifyingSection: some View {
-        Section {
-            ForEach(MobileGestaltKey.Identifying.allCases, id: \.self) { key in
+    func section(_ title: String, keys: [String]) -> some View {
+        var keys = keys
+        if !searchText.isEmpty {
+            keys = keys.filter { key in
+                key.lowercased().contains(searchText.lowercased())
+            }
+        }
+
+        return DisclosureGroup {
+            ForEach(keys, id: \.self) { key in
                 HStack {
-                    Text(key.rawValue)
+                    Text(key)
                     Spacer()
                     Text(SMGCopyAnswerAsString(key) ?? "nil")
                 }
             }
-        } header: {
-            Text("Identifying")
-        }
-    }
-
-    var bluetoothSection: some View {
-        Section {
-            ForEach(MobileGestaltKey.Bluetooth.allCases, id: \.self) { key in
-                HStack {
-                    Text(key.rawValue)
-                    Spacer()
-                    Text(SMGCopyAnswerAsString(key) ?? "nil")
-                }
+        } label: {
+            HStack {
+                Text(title)
+                Spacer()
+                Text("\(keys.count)")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
             }
-        } header: {
-            Text("Bluetooth")
-        }
-    }
-
-    var batterySection: some View {
-        Section {
-            ForEach(MobileGestaltKey.Battery.allCases, id: \.self) { key in
-                HStack {
-                    Text(key.rawValue)
-                    Spacer()
-                    Text(SMGCopyAnswerAsString(key) ?? "nil")
-                }
-            }
-        } header: {
-            Text("Battery")
-        }
-    }
-
-    var basebandSection: some View {
-        Section {
-            ForEach(MobileGestaltKey.Baseband.allCases, id: \.self) { key in
-                HStack {
-                    Text(key.rawValue)
-                    Spacer()
-                    Text(SMGCopyAnswerAsString(key) ?? "nil")
-                }
-            }
-        } header: {
-            Text("Baseband")
-        }
-    }
-
-    var telephonySection: some View {
-        Section {
-            ForEach(MobileGestaltKey.Telephony.allCases, id: \.self) { key in
-                HStack {
-                    Text(key.rawValue)
-                    Spacer()
-                    Text(SMGCopyAnswerAsString(key) ?? "nil")
-                }
-            }
-        } header: {
-            Text("Telephony")
-        }
-    }
-
-    var deviceSection: some View {
-        Section {
-            ForEach(MobileGestaltKey.Device.allCases, id: \.self) { key in
-                HStack {
-                    Text(key.rawValue)
-                    Spacer()
-                    Text(SMGCopyAnswerAsString(key) ?? "nil")
-                }
-            }
-        } header: {
-            Text("Device")
-        }
-    }
-
-    var capabilitySection: some View {
-        Section {
-            ForEach(MobileGestaltKey.Capability.allCases, id: \.self) { key in
-                HStack {
-                    Text(key.rawValue)
-                    Spacer()
-                    Text(SMGCopyAnswerAsString(key) ?? "nil")
-                }
-            }
-        } header: {
-            Text("Capability")
-        }
-    }
-
-    var regionalSection: some View {
-        Section {
-            ForEach(MobileGestaltKey.RegionalBehavior.allCases, id: \.self) { key in
-                HStack {
-                    Text(key.rawValue)
-                    Spacer()
-                    Text(SMGCopyAnswerAsString(key) ?? "nil")
-                }
-            }
-        } header: {
-            Text("Regional")
-        }
-    }
-
-    var wirelessSection: some View {
-        Section {
-            ForEach(MobileGestaltKey.Wireless.allCases, id: \.self) { key in
-                HStack {
-                    Text(key.rawValue)
-                    Spacer()
-                    Text(SMGCopyAnswerAsString(key) ?? "nil")
-                }
-            }
-        } header: {
-            Text("Wireless")
-        }
-    }
-
-    var faceTimeSection: some View {
-        Section {
-            ForEach(MobileGestaltKey.FaceTime.allCases, id: \.self) { key in
-                HStack {
-                    Text(key.rawValue)
-                    Spacer()
-                    Text(SMGCopyAnswerAsString(key) ?? "nil")
-                }
-            }
-        } header: {
-            Text("FaceTime")
-        }
-    }
-
-    var moreDeviceSection: some View {
-        Section {
-            ForEach(MobileGestaltKey.MoreDeviceCapability.allCases, id: \.self) { key in
-                HStack {
-                    Text(key.rawValue)
-                    Spacer()
-                    Text(SMGCopyAnswerAsString(key) ?? "nil")
-                }
-            }
-        } header: {
-            Text("MoreDeviceCapability")
         }
     }
 }
@@ -188,5 +79,11 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension CaseIterable where Self: MobileGestaltKeyProtocol {
+    static var allStringKeys: [String] {
+        allCases.map(\.rawValue)
     }
 }
